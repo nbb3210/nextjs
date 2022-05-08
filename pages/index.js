e17926fc9a15ea0ecd6056e2aa422a6330686c0a
unknown <nbb3210@gmail.com>
2022-05-08 19:38:38 +0800

    try my new pc

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         xuqiangWelcome to <a href="https://nextjs.org">Next.js!</a>
+         xuqiang  Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
