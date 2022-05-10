86a7f050b3d590cc5829fd790d017891eed5fb7a
nbb3210 <nbb3210@gmail.com>
2022-05-10 10:52:33 +0800

    test: ubuntu

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-         xuqiang  Welcome to <a href="https://nextjs.org">Next.js!</a>
+         xuqiang  Welcome to <a href="https://nextjs.org">Next.js!(from ubuntu)</a>
         </h1>
 
         <p className={styles.description}>
